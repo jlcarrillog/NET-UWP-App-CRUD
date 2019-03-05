88ebf4d715b3fdb8cf19f59be3fc14dd199b995b
Luis Carrillo <juan_carrillog@my.unitec.edu.mx>
2019-03-05 16:08:27 -0600

    Arquitectura

--- App/App/Views/Empleados/Empleados.xaml.cs
+++ App/App/Views/Empleados/Empleados.xaml.cs
@@ -6,7 +6,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 
-namespace App
+namespace App.Views
 {
     public sealed partial class Empleados : Page
     {
--- App/App/Views/Empleados/EmpleadosEdit.xaml.cs
+++ App/App/Views/Empleados/EmpleadosEdit.xaml.cs
@@ -5,7 +5,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-namespace App
+namespace App.Views
 {
     public sealed partial class EmpleadosEdit : Page
     {
